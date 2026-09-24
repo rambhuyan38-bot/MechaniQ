4225a7c78010be576b8c4bbf6a5794248265543b
NexType AI <rambhuyan38@gmail.com>
2026-09-24 14:14:38 +0530

    AI Create: lib/utils/app_colors.dart

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBackground = Color(0xFF0A0E17);
+  static const Color cardBackground = Color(0xFF131A26);
+  static const Color primaryNeonBlue = Color(0xFF00E5FF);
+  static const Color secondaryNeonOrange = Color(0xFFFF8F00);
+  static const Color errorNeonRed = Color(0xFFFF1744);
+  static const Color successNeonGreen = Color(0xFF00E676);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFF8A99AD);
+  static const Color borderCyan = Color(0x3300E5FF);
+  static const Color shadowCyan = Color(0x1F00E5FF);
+}
